45092e4710148472214d0ca9e703828997b8a5c9
Joe Peralta <jperalta11@myseneca.ca>
2024-08-30 14:17:27 -0400

    Cleanup the navbrand

--- src/components/Navbrand.jsx
+++ src/components/Navbrand.jsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Navbrand() {
   return (
-    <h1 className="navbrand">
-      <Link className="link font-bold text-4xl" href="/">
-        Joe.
+    <h1 className="navbrand w-full">
+      <Link className="link font-bold text-2xl" href="/">
+        Joeward Peralta
       </Link>
     </h1>
   );
