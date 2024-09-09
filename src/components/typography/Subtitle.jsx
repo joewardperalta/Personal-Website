df77a6503a2a7e4aa79baabda965dd9ccd07d6d0
Joe Peralta <jperalta11@myseneca.ca>
2024-09-09 17:24:10 -0400

    Change color of the subtitle

--- src/components/typography/Subtitle.jsx
+++ src/components/typography/Subtitle.jsx
@@ -1,3 +1,7 @@
 export default function Subtitle({ children }) {
-  return <p className="mb-10 mx-auto max-w-[600px] lg:mx-0">{children}</p>;
+  return (
+    <p className="mb-10 mx-auto max-w-[600px] text-slate-500 lg:mx-0">
+      {children}
+    </p>
+  );
 }
