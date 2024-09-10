fb5184f1cb163ff4464179158236650bdb7b03b6
Joe Peralta <jperalta11@myseneca.ca>
2024-09-10 13:43:08 -0400

    Created a constant js file to store all variables that will not change

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const portfolioProjects = [
+  {
+    id: 1,
+    title: "Apostolic Sanctuary of Canada",
+    description: "Church Website",
+    image: "/images/TASC Website.png",
+    link: "https://www.apostolicsanctuarycanada.ca/",
+  },
+  {
+    id: 2,
+    title: "Insideout Window Coverings",
+    description: "Business Website",
+    image: "/images/Insideout Window Coverings Website.png",
+    link: "https://www.insideoutwindowcoverings.ca/",
+  },
+  {
+    id: 3,
+    title: "Bahay Kubo Child Centre",
+    description: "School Website",
+    image: "/images/arnel-hasanovic-MNd-Rka1o0Q-unsplash.jpg",
+    link: "/",
+  },
+];
