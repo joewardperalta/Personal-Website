6bc15362a253092ea6eb1e2562e1049a84e74556
Joe Peralta <jperalta11@myseneca.ca>
2024-09-10 13:42:33 -0400

    Created Product Card component

---
+++ src/components/cards/ProductCard.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ProductCard({ image, title, description, link }) {
+  return (
+    <div className="w-full">
+      <div className="h-80 mb-7">
+        <Image
+          className="w-full h-full rounded-xl object-cover object-top"
+          src={image}
+          width={2000}
+          height={2000}
+          alt="product"
+        />
+      </div>
+      <div className="flex justify-between items-center">
+        <div>
+          <h5 className="font-semibold text-lg text-slate-900">{title}</h5>
+          <p className="text-slate-600">{description}</p>
+        </div>
+        <Link
+          className="rounded-full bg-primary p-4 w-[50px] h-[50px]"
+          href={link}
+          target="_blank"
+        >
+          <Image
+            className="w-full h-full"
+            src="/images/up-right-arrow.png"
+            width={64}
+            height={64}
+            alt="link"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
