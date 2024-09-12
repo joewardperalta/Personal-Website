e14dd86ba4d25edfa13623040ba3ecdce9b9d87b
Joe Peralta <jperalta11@myseneca.ca>
2024-09-12 18:40:45 -0400

    Change the input text color to white

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,7 +1,7 @@
 export default function Input({ type, id, name, placeholder, required }) {
   return (
     <input
-      className="px-4 py-3 text-sm rounded-xl bg-transparent border border-slate-500 text-slate-400 placeholder:text-slate-500 focus-visible:outline-none focus-visible:border-slate-400 md:text-base lg:px-7 lg:py-5"
+      className="px-4 py-3 text-sm rounded-xl bg-transparent border border-slate-500 placeholder:text-slate-500 focus-visible:outline-none focus-visible:border-slate-400 md:text-base lg:px-7 lg:py-5"
       type={type}
       id={id}
       name={name}
