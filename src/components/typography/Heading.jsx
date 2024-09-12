eeb639f3b79cf229430ea3f3dfc33b414fbc11b0
Joe Peralta <jperalta11@myseneca.ca>
2024-09-12 15:09:59 -0400

    Fixed heading size when on small device

--- src/components/typography/Heading.jsx
+++ src/components/typography/Heading.jsx
@@ -1,6 +1,10 @@
 export default function Heading({ children, className }) {
   return (
-    <h2 className={"font-bold text-4xl mb-4 text-slate-900 " + className}>
+    <h2
+      className={
+        "font-bold mb-4 text-slate-900 text-3xl lg:text-4xl " + className
+      }
+    >
       {children}
     </h2>
   );
