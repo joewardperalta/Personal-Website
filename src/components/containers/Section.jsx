65177ec07f3c989d57780cf447a5950f4b8f84e1
Joe Peralta <jperalta11@myseneca.ca>
2024-09-12 15:12:56 -0400

    Fixed section spacing when on lower device

--- src/components/containers/Section.jsx
+++ src/components/containers/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section({ children, className, id }) {
   return (
-    <section className={"py-32 " + className} id={id}>
+    <section className={"py-20 lg:py-32 " + className} id={id}>
       {children}
     </section>
   );
