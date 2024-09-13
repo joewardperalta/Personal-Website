e10961a8d19ef05601ed5d0163c0fc16201e2930
Joe Peralta <jperalta11@myseneca.ca>
2024-09-12 20:23:18 -0400

    Applied a contact form reset so that the input fields gets reset whenever the user submits the form. I also added a success message to notify the user that the email was successfully sent

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import PrimaryButton from "../buttons/PrimaryButton";
 import Input from "./Input";
 import Textarea from "./Textarea";
 
 export default function ContactForm() {
+  const [submitted, setSubmitted] = useState(false);
+
   async function handleSubmit(event) {
     event.preventDefault();
 
     const formData = new FormData(event.target);
+
+    const contactForm = document.getElementById("contact-form");
+    contactForm.reset(); // reset input fields
 
     try {
       const response = await fetch("/api/contact", {
@@ -15,10 +21,12 @@ export default function ContactForm() {
       });
 
       if (!response.ok) {
+        setSubmitted(false);
         console.log("falling over");
         throw new Error(`response status: ${response.status}`);
       }
 
+      setSubmitted(true);
       const responseData = await response.json();
       console.log(responseData.message);
     } catch (error) {
@@ -27,7 +35,11 @@ export default function ContactForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col w-full gap-5">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col w-full gap-5"
+      id="contact-form"
+    >
       <Input
         type="text"
         id="name"
@@ -56,7 +68,10 @@ export default function ContactForm() {
         name="message"
         placeholder="Write Your Message"
       />
-      <PrimaryButton type="submit">Send Email</PrimaryButton>
+      <div className="space-x-4">
+        <PrimaryButton type="submit">Send Email</PrimaryButton>
+        {submitted && <span className="text-green-400">Sent Successfull</span>}
+      </div>
     </form>
   );
 }
